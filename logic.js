b2d3d4c80d2da740b1363c25771dc473d01fad4e
PratyushTheCoder <pratyush.jha299gamil.com>
2024-03-04 20:27:30 +0530

    visual changes

--- logic.js
+++ logic.js
@@ -1,3 +1,11 @@
+const send_btn=document.getElementById('send_btn');
+const username=document.getElementById('name');
+const email=document.getElementById("email");
+const message=document.getElementById("message");
 
-
+send_btn.onclick=()=>{
+    username.innerText="";
+    email.innerText="";
+    message.innerText="";
+}
 
